426596a582cb115b39c810f2bacb35f21746a5ef
jiri_otoupal <Lkw2Rn89p7yNJUMRD4fv>
2022-12-31 02:02:49 +0100

    File watch and sequence exec
    Signed-off-by: jiri_otoupal <Lkw2Rn89p7yNJUMRD4fv>

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     include_package_data=True,
     python_requires=">=3.7,<4",
     install_requires=["click", "rich", "inquirerpy", "eventlet", "lastversion",
-                      "requests", "semantic_version", "escapejson"],
+                      "requests", "semantic_version", "escapejson", "watchdog"],
     extras_require={
         "dev": ["black==22.*"],
     },
